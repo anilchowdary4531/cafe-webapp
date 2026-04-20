0c4a7c2f4449f9fb0bd949c16d5665cc42a7c329
Anil Chowdary <anilchowdarya8@gmail.com>
2026-04-20 15:21:04 +0530

    Fixing jwt authentication

--- backend-microservices/menu-service/src/main/java/com/cafe/menu/repository/MenuItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cafe.menu.repository;
-
-import com.cafe.menu.entity.MenuItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
-    List<MenuItem> findAllByOrderByIdAsc();
-}
--- backend-microservices/user-service/src/main/java/com/cafe/user/repository/CustomerSessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cafe.user.repository;
-
-import com.cafe.user.entity.CustomerSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface CustomerSessionRepository extends JpaRepository<CustomerSession, String> {
-    Optional<CustomerSession> findByTableLabel(String tableLabel);
-}
